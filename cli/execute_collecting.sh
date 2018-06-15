0a25bfd46a92596197aa562d054e50911080738d
Imran Nababan <imzers@gmail.com>
2018-06-15 20:49:33 +0700

    rf

--- cli/execute_collecting.sh
+++ cli/execute_collecting.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-/usr/local/bin/lsphp /home/dhfwjaco/public_html/index.php cli/Cryptocurrency_ticker_data_update execute_update_crypto_data
+/usr/local/bin/php /home/dhfwjaco/public_html/index.php cli/Cryptocurrency_ticker_data_update execute_update_crypto_data
