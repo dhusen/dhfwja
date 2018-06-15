4dd96c339f3a028d0baaecc7a8871ce30ce3b093
Imran Nababan <imzers@gmail.com>
2018-06-15 14:38:58 +0700

    add sh bash

---
+++ cli/execute_collecting.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+php /home/dhfwjaco/public_html/index.php cli/Cryptocurrency_ticker_data_update execute_update_crypto_data
---
+++ cli/execute_ticker_cryptocurrency.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+php /home/dhfwjaco/public_html/index.php cli/Cryptocurrency_ticker_data_update running_cryptocurrency_cli
