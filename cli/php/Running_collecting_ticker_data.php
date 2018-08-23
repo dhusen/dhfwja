63dba99c37e202dcb957b77db5b40959d2194db6
Imran Nababan <imzers@gmail.com>
2018-08-23 12:50:23 +0700

    dd

--- cli/php/Running_collecting_ticker_data.php
+++ cli/php/Running_collecting_ticker_data.php
@@ -5,4 +5,4 @@ include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Cryptocurrency_ticker_da
 $Cryptocurrency_ticker_data_update = new Cryptocurrency_ticker_data_update();
 
 $Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(1); // Kraken
-//$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(2); // Bitcoin Indonesia
+$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(2); // Bitcoin Indonesia
