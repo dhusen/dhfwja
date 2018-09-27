236204d489124b3c6abf0bc4ad2407740b24f03c
Imran Nababan <imzers@gmail.com>
2018-09-27 13:51:32 +0700

    make it every 30 seconds

--- cli/php/Running_collecting_ticker_data.php
+++ cli/php/Running_collecting_ticker_data.php
@@ -4,5 +4,11 @@ include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Cryptocurrency_ticker_da
 
 $Cryptocurrency_ticker_data_update = new Cryptocurrency_ticker_data_update();
 
-$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(1); // Kraken
-$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(2); // Bitcoin Indonesia
+# Make it every 30 seconds to collect data
+# (5 * 60) / 30 = 10
+for ($i = 0; $i < 10; $i++) {
+	$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(1); // Kraken
+	sleep(5);
+	$Cryptocurrency_ticker_data_update->update_cryptocurrency_ticker(2); // Bitcoin Indonesia
+	sleep(5);
+}
